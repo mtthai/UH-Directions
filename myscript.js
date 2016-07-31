cf75248d97b49e6a4027f260d9cf4d66b53087d7
Marvin Thai <marvinthai@yahoo.com>
2016-07-31 02:36:14 -0500

    Updated UI

--- myscript.js
+++ myscript.js
@@ -4,8 +4,8 @@ function initMap() {
   var directionsService = new google.maps.DirectionsService;
       
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 15,
-    center: {lat: 29.7189, lng: -95.3392},
+    zoom: 16,
+    center: {lat: 29.720072, lng: -95.342696},
     mapTypeControl: false,
     mapTypeControlOptions: {
         style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
@@ -34,7 +34,8 @@ function initMap() {
   
   var ctaLayer = new google.maps.KmlLayer({
           url: 'https://www.dropbox.com/s/jbjk0sh3wmov01v/U%20of%20H%20parking%20lot%20and%20traffic%20status.kmz?dl=1',
-          map: map
+          map: map,
+          preserveViewport: true
   });
 }
 
